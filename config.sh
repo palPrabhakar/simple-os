3e6aa945fb6771e742dcf79b2e44729b6db998f2
Prabhakar Pal <prabhakar.pal00@gmail.com>
2024-11-10 22:03:36 +0530

    small change in makefile

--- config.sh
+++ config.sh
@@ -14,7 +14,7 @@ export BOOTDIR=/boot
 export LIBDIR=$EXEC_PREFIX/lib
 export INCLUDEDIR=$PREFIX/include
 
-export CFLAGS='-O0 -fno-optimize-sibling-calls'
+export CFLAGS='-O2 -fno-optimize-sibling-calls'
 export CPPFLAGS=''
 
 # Configure the cross-compiler to use the desired system root.
